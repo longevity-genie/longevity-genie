52bae1c0b7aa3a7f0dc915c3c099f8238624df57
Maria Koval <marfantyk@gmail.com>
2023-05-21 22:40:33 +0300

    added function for preparation of clinvar db

--- genie/prepare.py
+++ genie/prepare.py
@@ -99,6 +99,38 @@ def prepare_coronary(locations: Locations):
     print("written to locations.coronary_text")
     return for_index
 
+def prepare_clinvar(locations: Locations):
+    query= "SELECT * FROM ncbi_clinvar;"
+    df = get_query_df(locations.clinvar, query)
+    source = (pl.lit("https://www.ncbi.nlm.nih.gov/clinvar/variation/") + pl.col("clinvar_id")).alias("source")
+    text_col: pl.Expr = (
+                pl.col("rsid") + pl.lit(" is located in ") +  pl.col("chrom") +
+                pl.lit(" with position ")  + pl.col("pos") +
+                pl.lit(" which is in the gene ") + pl.col("symbol") +
+                pl.lit(". This gene has the following description: ")+ pl.col("gene_description") +
+                pl.lit(" This SNP can be associated with ") +pl.col("disease_names")+
+                pl.lit(" with \"") + pl.col("sig") + pl.lit("\" clinical significance") +
+                pl.lit(" and with the review status: ") + pl.col("rev_stat") +
+                pl.lit(". ") + pl.col("rsid") + pl.lit("has the following disease_refs: ") +
+                pl.col("disease_refs") +
+                pl.lit(". Source: ") + source
+    ).alias("text")
+    for_index = df.select([
+                            pl.col("rsid"),
+                            pl.col("chrom"),
+                            pl.col("pos"),
+                            pl.col("symbol"),
+                            pl.col("gene_description"),
+                            pl.col("disease_names"),
+                            pl.col("rev_stat"),
+                            pl.col("sig"),
+                            pl.col("clinvar_id").cast(pl.Utf8),
+                            source,
+                            text_col])
+    for_index.write_csv(locations.clinvar_text, sep="\t")
+    print("written to locations.clinvar_text")
+    return for_index
+
 def with_papers_incremental(folder: Optional[Path] = None, skip_existing: bool = True):
     papers: list[Path] = traverse(folder, lambda p: "pdf" in p.suffix)
     print(f"indexing {len(papers)} papers")
