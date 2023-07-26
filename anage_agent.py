9849d19906869c292c8096de009f0a16ede4eb14
Alex-Karmazin <karmazinalex@gmail.com>
2023-07-26 07:57:27 +0300

    added first version of anage agent

--- anage_agent.py
+++ anage_agent.py
@@ -1,6 +1,6 @@
 # dependency on python-Levenshtein, thefuzz, polars
 from dotenv import load_dotenv
-_ = load_dotenv("test.env", override=True)
+_ = load_dotenv("4_5884009898785640766.env", override=True)
 
 from langchain.agents import tool
 from langchain.agents import AgentType, initialize_agent
@@ -23,11 +23,10 @@ def longevity(search_text: str) -> str:
     def levenshtein_dist(struct: dict) -> int:
         return max(fuzz.partial_ratio(struct["Species"], search_text), fuzz.partial_ratio(struct["Common name"], search_text))
 
-    frame = pl.read_csv("species_age.csv")
-    frame = frame.with_columns((pl.col("Species") + " " + pl.col("Common name")).alias("name"))
+    frame = pl.read_csv("data/csv_bases/species_age.csv")
     frame = frame.select([pl.struct(["Species", "Common name"]).apply(levenshtein_dist).alias("dist"), "Species", "Common name",
                           "Maximum longevity (yrs)"])
-    frame = frame.sort(by="dist", reverse=True).select(["Species", "Common name", "Maximum longevity (yrs)"])
+    frame = frame.sort(by="dist", descending=True).select(["Species", "Common name", "Maximum longevity (yrs)"])
 
     return frame.head(8)
 
